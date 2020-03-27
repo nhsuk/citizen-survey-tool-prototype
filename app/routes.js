c0678bcf55f60d174d98aa7b9d081a736b1ac296
Tom Doughty <thomashdoughty@gmail.com>
2020-03-27 18:02:09 +0000

    Add better comments to routing

--- app/routes.js
+++ app/routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 
 // Add your routes here - above the module.exports line
 router.post('/latest/when-symptoms-started', (req, res, next) => {
-	if (
-		// Skip no syptoms 
-		(req.session.data['symptoms'] === "No - I do not have any symptoms") 
+	// Skip symptoms date if no symptoms
+	if ((req.session.data['symptoms'] === "No - I do not have any symptoms")
+		// Don't interfere with POST middleware if user has reached summary before 
 		&& !req.session.data.household) {
 	  res.redirect('/latest/health-conditions');
 	}
