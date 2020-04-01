888c5cc3104b2cbcef922c6b1c94af4cfc0a5bfd
davidhunter08 <thebeakking@gmail.com>
2020-04-01 11:29:41 +0100

    add roting to return journeys

--- app/routes.js
+++ app/routes.js
@@ -10,4 +10,20 @@ router.post('/latest/when-did-this-start', (req, res, next) => {
 	next();
 });
 
+// Add your routes here - above the module.exports line
+router.post('/latest/return-with-data/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/latest/return-with-data/check-your-answers');
+	}
+	next();
+});
+
+// Add your routes here - above the module.exports line
+router.post('/latest/return-without-data/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/latest/return-without-data/check-your-answers');
+	}
+	next();
+});
+
 module.exports = router;
