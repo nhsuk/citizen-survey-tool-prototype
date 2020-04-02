08e89e26a6c54dfabebedaac1bf4aeff83f94e63
davidhunter08 <thebeakking@gmail.com>
2020-04-02 22:39:41 +0100

    update

---
+++ CHANGELOG.md
@@ -0,0 +1,44 @@
+# Citizen Survey Prototype Changelog
+
+## 1.0.2 - Mar 29, 2020
+
+:wrench: **Fixes**
+- Content updates based on feedback ([JIRA Ticket SIF-68](https://jira.service.nhs.uk/browse/SIF-68))
+- Remove `cookies` paragraph in footer
+- Add `cookies` link to list of links in the footer
+
+## 1.0.1 - Mar 28, 2020
+
+:new: **New features**
+
+- Add `nhsuk-inset-text` variant `nhsuk-inset-text--attention` and added to `/start` and `/done` pages - Sass in `/app/assets/sass/main.scss`
+
+Start page
+- Change `/health-conditions` page name to `/health-problems`
+- Update content
+
+Confirmation page
+- Change `/confirmation` page name to `/done`
+
+Current symptoms page
+- Change page name `current-symptoms` to `how-are-you-feeling`
+- Update page title, hint text and checkbox
+- Note: `"I am feeling OK"` answer replaced `"No - I do not have any symptoms"` but still routes to `health-problems`
+
+When symptoms started page
+- Change page name `when-symptoms-started` to `when-did-this-start`
+- Update page title and hint text
+
+Health conditions page
+- Change page name `health-conditions` to `health-problems`
+
+Check your answers page
+- Change heading `Coronavirus symptoms` to `Your experience`
+- Change `Current symptoms` to `How are you feeling?`
+- Change `Date symptoms started` to `When did this start?`
+
+## 1.0.0 - Mar 27, 2020
+
+:tada: **Official release of the Citizen Survey Prototype**
+
+- v1.0.0 of the Citizen Survey Prototype!
---
+++ app/routes.js
@@ -0,0 +1,46 @@
+// External dependencies
+const express = require('express');
+const router = express.Router();
+
+// Skip date of symptoms if no symptoms - LATEST
+router.post('/latest/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/latest/health-problems');
+	}
+	next();
+});
+
+// Skip date of symptoms if no symptoms - RETURN V2
+router.post('/return-v2/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/return-v2/health-problems');
+	}
+	next();
+});
+
+// Return journey - skip to check answers - RETURN V2
+router.post('/return-v2/return/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/return-v2/return/check-your-answers');
+	}
+	next();
+});
+
+
+// Return journey - skip to check answers - RETURN V1
+router.post('/return-v1/return-with-data/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/return-v1/return-with-data/check-your-answers');
+	}
+	next();
+});
+
+// Return journey - skip to check answers - RETURN V1
+router.post('/return-v1/return-without-data/when-did-this-start', (req, res, next) => {
+	if (req.session.data['symptoms'] === "I am feeling OK"){
+	  res.redirect('/return-v1/return-without-data/check-your-answers');
+	}
+	next();
+});
+
+module.exports = router;
